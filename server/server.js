048896fcadbd7c3213a6d8610a3298f61992867d
Cuthbert20 <knowltonspencer@gmail.com>
2019-08-08 10:47:16 -0600

    working on setup

--- server/server.js
+++ server/server.js
@@ -1,3 +1,29 @@
 const express = require('express')
 const app = express()
 require('dotenv').config()
+const massive = require("massive")
+const session = require('express-session')
+const { SERVER_PORT, CONNECTION_STRING, SESSION_SECRET } = process.env
+
+
+
+app.use(express.json())
+app.use(session({
+    secret: SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 7
+    }
+}))
+
+massive(CONNECTION_STRING).then(db => {
+    app.set('db', db)
+
+
+app.listen(SERVER_PORT, () => console.log(`${SERVER_PORT} is now listening`))
+
+}).catch(err => console.log(`not accessing db stu`))
+
+
+
